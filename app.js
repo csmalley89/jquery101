0a434236b515943cabfbfd4a5878f54d320291c4
Chris Smalley <chrism.smalley@gmail.com>
2016-08-01 12:06:02 -0500

    finished live coding with instructor

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ console.log('jquery 101')
 
 $(document).ready(function () {
   console.log('I am ready')
-
 });
 
-console.log('bottom of file')
+$(document).ready(function() {
+  // All the JavaScript that depends on jQuery will be written here
+
+  // Select the article DOM element by its id attribute
+  var article = $("#top-article");
+  console.log("article", article);
+});
+
+$('h1').click(function () {
+  console.log("CLicked h1")
+})
